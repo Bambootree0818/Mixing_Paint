895bc794ad3c2400d4e73a005853dd756563ebe8
Bambootree0818 <s2310060@jaist.ac.jp>
2024-07-05 18:26:53 +0900

    データセット関数実装

--- MixingPaint.py
+++ MixingPaint.py
@@ -18,6 +18,52 @@ for file_name in os.listdir(directory):
         print(file_name)
         sample_data[file_name] = read_sample(file_path)
 
-raito = input("raito : ")
+#raito = input("raito : ")
 
-print(sample_data[paint_name + "_" + raito])
+#print(sample_data[paint_name + "_" + raito][0])
+
+wi_theta = {}
+wi_phi = {}
+wo_theta = {}
+wo_phi = {}
+ref_b ={}
+ref_g = {}
+ref_r = {}
+
+#パラメータをセット
+def set_parameter(sample_data):
+    for raito in sample_data:
+        print(raito)
+        for row in sample_data[raito]:
+            
+            if raito not in wi_theta:
+                wi_theta[raito] = []
+            wi_theta[raito].append(float(row[0]))
+
+            if raito not in wi_phi:
+                wi_phi[raito] = []
+            wi_phi[raito].append(float(row[1]))
+
+            if raito not in wo_theta:
+                wo_theta[raito] = []
+            wo_theta[raito].append(float(row[2]))
+
+            if raito not in wo_phi:
+                wo_phi[raito] = []
+            wo_phi[raito].append(float(row[3]))
+
+            if raito not in ref_b:
+                ref_b[raito] = []
+            ref_b[raito].append(float(row[4]))
+
+            if raito not in ref_g:
+                ref_g[raito] = []
+            ref_g[raito].append(float(row[5]))
+
+            if raito not in ref_r:
+                ref_r[raito] = []
+            ref_r[raito].append(float(row[6]))
+
+set_parameter(sample_data)
+
+#print(ref_b["B&R_1&9"])
